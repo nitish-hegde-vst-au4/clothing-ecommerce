37f8bd9507ada7fb533cd402d36fe8e2e610b90a
Nitish <nitishh21@yahoo.com>
2020-05-24 11:50:56 +0530

    Created Homepage,Directory and MenuItem components

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Homepage.scss';
+import Directory from '../../components/Directory/Directory';
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
