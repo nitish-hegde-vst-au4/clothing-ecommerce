54ae27c468f212955eb8d5dd90bdbe90bc2a0884
Nitish <nitishh21@yahoo.com>
2020-05-25 11:31:08 +0530

    Refactored CollectionPreview and added Header component with respective page links

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import Shop from './pages/Shop/Shop';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Homepage} />;
-      <Route path='/shop' component={Shop} />;
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />;
+        <Route path='/shop' component={Shop} />;
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link to='/shop' className='option'>
+          SHOP
+        </Link>
+        <Link to='/contact' className='option'>
+          CONTACT
+        </Link>
+        <Link to='/signin' className='option'>
+          SIGNIN
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
