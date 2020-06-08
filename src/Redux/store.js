363e0885e851f9b27ca69d906bb92195afba0676
Nitish <nitishh21@yahoo.com>
2020-06-08 14:15:20 +0530

    remove redux-devtools from production build

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import rootReducer from './rootReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 const middlewares = [thunk];
 
 export const store = createStore(
